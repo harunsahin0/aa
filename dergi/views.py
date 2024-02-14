cded76ffaf750525dcd2c9f77f0f3a860541d753
harunsahin0 <64380346+harunsahin0@users.noreply.github.com>
2024-02-15 00:33:34 +0300

    Update views.py

--- dergi/views.py
+++ dergi/views.py
@@ -3,27 +3,27 @@ from django.http import HttpResponse
 
 
 def anasayfa(request):
-    return render(request,'dergi\index.html')
+    return render(request,'dergi/index.html')
 
 def hakkinda(request):
-    return render(request,'dergi\hakkinda.html')
+    return render(request,'dergi/hakkinda.html')
 
 def amacvekapsam(request):
-    return render(request,'dergi\\amacvekapsam.html')
+    return render(request,'dergi/amacvekapsam.html')
 
 def kunye(request):
-    return render(request,'dergi\kunye.html')
+    return render(request,'dergi/kunye.html')
 
 def kurullar(request):
-    return render(request,'dergi\kurullar.html')
+    return render(request,'dergi/kurullar.html')
 
 def yayinpolistikasi(request):
-    return render(request,'dergi\yayinpolitikasi.html')
+    return render(request,'dergi/yayinpolitikasi.html')
 
 def etik(request):
-    return render(request,'dergi\etik.html')
+    return render(request,'dergi/etik.html')
 
 def iletisim(request):
-    return render(request,'dergi\iletisim.html')
+    return render(request,'dergi/iletisim.html')
 
 # Create your views here.
