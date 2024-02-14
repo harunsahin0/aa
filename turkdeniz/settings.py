89c6e92a8369caa3c483212829e5f750bf22ce7a
harunsahin0 <64380346+harunsahin0@users.noreply.github.com>
2024-02-15 00:20:56 +0300

    Update settings.py

--- turkdeniz/settings.py
+++ turkdeniz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q^3816_iav9r&qgbe1#+5$^4@1khz-g6q-7na46^cj!mb#g-eh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://turkdeniz0.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','https://turkdeniz0.onrender.com']
 
 
 # Application definition
